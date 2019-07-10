baff47054da50ac6575b08cde9e7f0e808c3b2e0
csunny <cfqsunny@163.com>
2019-07-10 18:55:30 +0800

    rm target

--- adder/target/debug/adder-d17a4554583202d5.d
+++
@@ -1,1 +0,0 @@
-/Users/magic/workspace/data/www/GitHub/learning_rust/adder/target/debug/adder-d17a4554583202d5: /Users/magic/workspace/data/www/GitHub/learning_rust/adder/src/lib.rs
--- adder/target/debug/deps/adder-7ad2099aa627e844.d
+++
@@ -1,7 +0,0 @@
-/Users/magic/workspace/data/www/GitHub/learning_rust/adder/target/debug/deps/adder-7ad2099aa627e844.rmeta: src/lib.rs
-
-/Users/magic/workspace/data/www/GitHub/learning_rust/adder/target/debug/deps/libadder-7ad2099aa627e844.rlib: src/lib.rs
-
-/Users/magic/workspace/data/www/GitHub/learning_rust/adder/target/debug/deps/adder-7ad2099aa627e844.d: src/lib.rs
-
-src/lib.rs:
