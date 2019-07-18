778ccbfc10f37bf749c509d01aee671b0bce3819
csunny <cfqsunny@163.com>
2019-07-18 14:44:06 +0800

    oop

---
+++ oop/src/lib.rs
@@ -0,0 +1,50 @@
+pub struct AveragedCollection {
+    list: Vec<i32>,
+    average: f64,
+}
+
+impl AveragedCollection {
+    // add code here
+    pub fn add(&mut self, value: i32){
+        self.list.push(value);
+        self.update_average();
+    }
+
+    pub fn remove(&mut self) -> Option<i32>{
+        let result = self.list.pop();
+        match result {
+            Some(value) => {
+                self.update_average();
+                Some(value);
+            },
+            None => None,
+        }
+    }
+    pub fn average(&self) -> f64{
+        self.average
+    }
+
+    fn update_average(&mut self){
+        let total: i32 = self.list.iter().sum();
+        self.average = total as f64 / self.list.len() as f64;
+    }
+}
+
+pub trait Draw {
+    // add code here
+    fn draw(&self);
+}
+
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+
+impl Screen {
+    // add code here
+    pub fn run(&self){
+        for component in self.components.iter(){
+            component.draw();
+        }
+    }
+}
+
